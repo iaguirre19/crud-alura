4977e1d5344c91792dfed3ab53b76794fef02797
iaguirre19 <iran.agui1993@gmail.com>
2023-05-24 00:28:49 -0600

    actualizado

--- service/client-service.js
+++ service/client-service.js
@@ -1,5 +1,8 @@
-const crearNuevaLinea = (nombre, email) => {
-    const linea = document.createElement('tr');
+const crearNuevaLinea = (nombre, email) => { // esta funcion recibe dos argumentos nombre y email
+
+    const linea = document.createElement('tr'); // Esto crear una etiqueta "tr" con create element
+    
+    // esto crea un contenido html, tambien se puede hacer de otra manera creadolos elementos con create element y anidandolos con append chield
     const contenido = `
     <td class="td" data-td>${nombre}</td>
     <td>${email}</td>
@@ -28,10 +31,33 @@ const crearNuevaLinea = (nombre, email) => {
 
 const table = document.querySelector("[data-table]");
 
+const listaClientes = () => {
+  const promise = new Promise((resolve, reject) => {
+    const http = new XMLHttpRequest();
+    http.open("GET", "http://localhost:3000/perfil") // Envia una solicitud para obtener datos de una base de darps
+    
+    http.send(); // Envia a la base de datos
+    
+    http.onload = () => {  //Esta funcion lo que hace es que con onload le dice que cuando termine de cargar la pagina por completo entonces con la variable data espera una respuesta del contenido de la base de datos.
+        const response = JSON.parse(http.response); // se usa el JSON.PARSE para que convierta lo queue recibe de la base de datos ya que lo recibe como texto. y ahora con esto nos va a regresar un objeto
+        
+        if(http.status >= 400){
+          reject(response)
+        }else{
+          resolve(response)
+        }
+    }
 
+  })
+  return promise
+}
 
-const http = new XMLHttpRequest();
-
+listaClientes().then((data) => {
+  data.forEach(perfil => {
+      const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email)
+      table.appendChild(nuevaLinea)
+  });
+}).catch((error) => alert("Ocurrio un error"))
 // ABRIR http (metodo, url)
 // CRUD --- Metodos
 // Creat ----- Post
@@ -42,20 +68,14 @@ const http = new XMLHttpRequest();
 
 
 
-http.open("GET", "http://localhost:3000/perfil") // Obtener la informacion de la base de datos  o el enlace con la base de datos
-
-http.send(); // Envia a la base de datos
-
-http.onload = () => {  //Esta funcion lo que hace es que con onload le dice que cuando termine de cargar la pagina por completo entonces con la variable data espera una respuesta del contenido de la base de datos.
-    const data = JSON.parse(http.response); // se usa el JSON.PARSE para que convierta lo queue recibe de la base de datos ya que lo recibe como texto. y ahora con esto nos va a regresar un arreglo
 
 
-    console.log(typeof(data))
 
-    data.forEach(perfil => {
-        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email)
-        table.appendChild(nuevaLinea)
-    });
-};
+
+    // data.forEach(perfil => {
+    //     const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email)
+    //     table.appendChild(nuevaLinea)
+    // });
+
 
 
